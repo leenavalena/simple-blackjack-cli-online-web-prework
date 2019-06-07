f756223fef14bee766ae0709cf32e266385fa359
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-07 01:31:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,5 +50,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  deal_card
+  display_card_total
+  prompt_user
+  get_user_input
+end_game
+initial_round
+hit
+invalid_command
 end
